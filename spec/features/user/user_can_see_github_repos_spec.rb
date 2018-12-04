c708bb8e2e2d82ce1353c80f8cf6254e43fe37b7
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-04 13:43:49 -0700

    adds feature test for testing github repos in user dashboard"

---
+++ spec/features/user/user_can_see_github_repos_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'User' do
+  it 'user can sign in' do
+    user = create(:user)
+    
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/dashboard'
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Github Repositories")
+    expect(page).to have_css(".repo", count: 5)
+    
+    within(first(".repo")) do 
+      expect(page).to have_css(".name")
+      expect(page).to have_link("trelora_consult_app")
+    end
+    click_link "trelora_consult_app"
+    expect(page).to have_content("Autumn-Martin/trelora_consult_app")
+  end
+end
