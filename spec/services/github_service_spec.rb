0d8c06977017ae76c48dd290401b4294c33c7343
Harper Bellows <harper.bellows@gmail.com>
2018-12-05 06:21:07 -0700

    refactor github service spec to limit repo returns to 5 down from 30

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -14,7 +14,7 @@ describe GithubService do
     github_data = service.get_repos
 
     expect(github_data).to be_a(Array)
-    expect(github_data.count).to eq(30)
+    expect(github_data.count).to eq(5)
     # expect(github_data.first[:name]).to eq("trelora_consult_app")
   end 
 end
