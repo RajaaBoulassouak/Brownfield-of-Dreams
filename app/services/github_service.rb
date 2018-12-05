8bf117e87510da8229b401d354b417f29e5d614f
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-05 13:03:55 -0700

    modifies request for github repos to work with user github token from database

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,7 +1,11 @@
 class GithubService
   
+  def initialize(token)
+    @token = token
+  end
+  
   def get_repos
-    get_json("/user/repos")
+    get_json("/user/repos?per_page=5")
   end
   
   private
@@ -12,7 +16,7 @@ class GithubService
 
   def conn
     Faraday.new(:url => 'https://api.github.com') do |faraday|
-      faraday.headers['Authorization'] = ENV['GITHUB_TOKEN']
+      faraday.headers['Authorization'] = "token #{@token}"
       faraday.adapter  Faraday.default_adapter
     end
   end
