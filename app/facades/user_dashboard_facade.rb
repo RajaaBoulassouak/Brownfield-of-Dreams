c1e2eaece633bb34a5481e8054f84bda406c54c4
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-04 18:30:30 -0700

    adds methods: followers, followers_result and names result method to be repos_result

--- app/facades/user_dashboard_facade.rb
+++ app/facades/user_dashboard_facade.rb
@@ -4,16 +4,26 @@ class UserDashboardFacade
   end
   
   def repos
-    result[0..4].map do |repo_data|
+    repos_result[0..4].map do |repo_data|
       Repo.new(repo_data)
     end
   end
   
-  private
-  def result
-    @result ||= github_service.get_repos
+  def followers
+    followers_result.map do |follower_data|
+      Follower.new(follower_data)
+    end
   end
   
+  private
+  def repos_result
+    @repos_result ||= github_service.get_repos
+  end
+  
+  def followers_result
+    @followers_result ||= github_service.get_followers
+  end
+
   def github_service
     GithubService.new
   end
