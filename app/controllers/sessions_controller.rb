a3316b4f827b3a41811f1f0c622b4be4564195f9
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-04 17:32:04 -0700

    changes config.assets.compile back to be false

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    require "pry"; binding.pry
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
