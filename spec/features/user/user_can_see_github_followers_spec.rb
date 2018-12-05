b42e707490fc0c98f306966eb0b1df36cd3b9bbe
Harper Bellows <harper.bellows@gmail.com>
2018-12-05 08:37:47 -0700

    strengthen github follower spec

--- spec/features/user/user_can_see_github_followers_spec.rb
+++ spec/features/user/user_can_see_github_followers_spec.rb
@@ -5,24 +5,22 @@ describe 'User visits /dashboard' do
     it 'displays user followers' do
       user = create(:user, token: ENV['hb_github_token'])
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      # As a logged in user
+
       visit '/dashboard'
-      # When I visit /dashboard
+
       expect(current_path).to eq(dashboard_path)
       
       expect(page).to have_content("Github")
-      # Then I should see a section for "Github"
       expect(page).to have_content("Followers")
-      # And under that section I should see another section titled "Followers"
-      expect(page).to have_link(" ")
-      # And I should see list of all followers with their handles linking to their Github profile
+      
+      within(first(".followers")) do
+        expect(page).to have_css(".follower-name")
+        expect(page).to have_link("andrewetobin")
+      end
+      click_link "andrewetobin"
+      expect(current_path).to eq("/users/andrewetobin")
     end
   end
 end
 
-# API endpoint documentation
-# https://developer.github.com/v3/users/followers/
 
-#   API call uses authenticated call: /user/followers instead of /users/:username/followers
-#   No hashes in the view.
-#   Uses objects in the view to represent the followers.
