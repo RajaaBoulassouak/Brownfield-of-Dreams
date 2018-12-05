b9db274ae43eb8c492aeaaf46b1fefbd065e52ae
Harper Bellows <harper.bellows@gmail.com>
2018-12-05 11:37:42 -0700

    add feature spec for user story 14 github following

--- spec/features/user/user_can_see_github_following_spec.rb
+++ spec/features/user/user_can_see_github_following_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'User visits /dashboard' do
   describe 'and sees Following section under Github section' do
     it 'displays the names of those the user is following' do
+
       user = create(:user, token: ENV['hb_github_token'])
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -22,15 +23,3 @@ describe 'User visits /dashboard' do
     end
   end
 end
-
-# As a logged in user
-# When I visit /dashboard
-# Then I should see a section for "Github"
-# And under that section I should see another section titled "Following"
-# And I should see list of users I follow with their handles linking to their Github profile
-# API endpoint documentation
-# https://developer.github.com/v3/users/followers
-
-#   API call uses authenticated call
-#   No hashes in the view.
-#   Uses objects in the view to represent the Github users.
