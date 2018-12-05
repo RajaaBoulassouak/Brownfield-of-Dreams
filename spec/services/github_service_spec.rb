61398c88530d117049c1acb30a56cdcebb028c42
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-05 15:18:16 -0700

    modifies syntax on github service and user dahsboard facade tests

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -7,12 +7,13 @@ describe GithubService do
     expect(service).to be_a(GithubService)
   end
 
-  it 'gets data' do 
+  it 'gets repos data' do 
     service = GithubService.new(ENV['GITHUB_TOKEN'])
-    github_data = service.get_repos
+    repos_data = service.get_repos
 
-    expect(github_data).to be_a(Array)
-    expect(github_data.count).to eq(5)
-    expect(github_data.first[:name]).to eq("trelora_consult_app")
+    expect(repos_data).to be_a(Array)
+    expect(repos_data.count).to eq(5)
+    expect(repos_data.first).to be_a(Hash)
+    expect(repos_data.first[:name]).to eq("trelora_consult_app")
   end 
 end
