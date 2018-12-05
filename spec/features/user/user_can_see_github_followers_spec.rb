1e6644c073fc94b599bf3e22f29fa7fd5773c8ef
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-04 18:16:36 -0700

    adds users github folllowers feature test (partly)

---
+++ spec/features/user/user_can_see_github_followers_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User can see a list of all their Github followers' do
+  scenario 'with the followers handles linking to their Github profile' do
+    user = create(:user)
+    
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/dashboard'
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Followers")
+    
+    within(first(".follower")) do 
+      expect(page).to have_css(".name")
+      # expect(page).to have_link()
+    end
+  
+    # click_link ""
+    # expect(current_path).to eq("")
+  end
+end
