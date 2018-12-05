aa53db808f7a5007c59c5215999e4a80d46b2af2
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-05 14:59:33 -0700

    adjusts service tests according new changes

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 describe GithubService do 
   it 'exists' do
-    service = GithubService.new 
+    service = GithubService.new(ENV['GITHUB_TOKEN'])
 
     expect(service).to be_a(GithubService)
   end
 
   it 'gets data' do 
-    service = GithubService.new 
+    service = GithubService.new(ENV['GITHUB_TOKEN'])
     github_data = service.get_repos
 
     expect(github_data).to be_a(Array)
-    expect(github_data.count).to eq(30)
+    expect(github_data.count).to eq(5)
     expect(github_data.first[:name]).to eq("trelora_consult_app")
   end 
 end
