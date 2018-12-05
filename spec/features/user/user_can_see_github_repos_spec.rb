99b6859abaf0bf9c360210aacf50e017f7b7406e
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-04 18:14:03 -0700

    modifies syntax in repos feature test and adjust view accordingly

--- spec/features/user/user_can_see_github_repos_spec.rb
+++ spec/features/user/user_can_see_github_repos_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
-describe 'User' do
-  it 'user can sign in' do
+feature 'User can see a list of 5 of their Github repositories' do
+  scenario 'with the name of each Repo linking to the repo on Github' do
     user = create(:user)
     
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit '/dashboard'
 
-    expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content("Github Repositories")
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_content("Github")
+    expect(page).to have_content("Repositories")
     expect(page).to have_css(".repo", count: 5)
     
     within(first(".repo")) do 
