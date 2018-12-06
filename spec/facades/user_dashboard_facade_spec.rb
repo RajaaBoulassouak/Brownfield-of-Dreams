a5eb6e85f0c5e641136ee88a0109b39f17eab9b0
Harper Bellows <harper.bellows@gmail.com>
2018-12-05 17:18:34 -0700

    add facade test for followers method

--- spec/facades/user_dashboard_facade_spec.rb
+++ spec/facades/user_dashboard_facade_spec.rb
@@ -23,4 +23,17 @@ describe UserDashboardFacade do
     expect(result.repos.first).to be_a(Repo)
     expect(result.repos.last).to be_a(Repo)
   end 
+  
+  it 'has followers' do 
+    user = create(:user)
+    github_token = GithubToken.create(token: ENV['USER_GITHUB_TOKEN_1'], user_id: user.id)
+  
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  
+    result = UserDashboardFacade.new(user)
+  
+    expect(result.followers).to be_a(Array)
+    expect(result.followers.first).to be_a(Follower)
+    expect(result.followers.last).to be_a(Follower)
+  end 
 end
