8076c0f5de032e2892e6c2a4590c50409ad5c335
Harper Bellows <harper.bellows@gmail.com>
2018-12-07 07:12:02 -0700

    update user model to save new github token info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save: ensure_github_token
+
   has_many :user_videos
   has_many :videos, through: :user_videos
   has_one :github_token
@@ -9,7 +11,22 @@ class User < ApplicationRecord
   enum role: [:default, :admin]
   has_secure_password
 
-  def self.create_with_omniauth(info)
-    create(name: info['name'])
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.email = auth.info.email
+      user.save!
+    end
+  end
+
+  private 
+  
+  def ensure_github_token
+    self.github_token = Github_token.find_or_create_by.tap do |user|
+      user.uid = auth.uid
+      user.provider = auth.provider
+      user.avatar_url = auth.info.image
+      user.username = auth.info.name
+      user.oauth_token = auth.credentials.token
+    end
   end
 end
