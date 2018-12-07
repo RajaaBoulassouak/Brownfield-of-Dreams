1c6a4bdd2e05218fdd62bc3ea631270ef64391d7
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-06 19:30:37 -0700

    adds session create method for github OAuth, saves token in db, gem WIP

--- app/controllers/github/sessions_controller.rb
+++ app/controllers/github/sessions_controller.rb
@@ -1,5 +1,21 @@
 class Github::SessionsController < ApplicationController
   
   def create
-    require "pry"; binding.pry
+    response = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{ENV['GITHUB_CLIENT_ID']}&client_secret=#{ENV['GITHUB_CLIENT_SECRET']}&code=#{params[:code]}")
+    pairs = response.body.split("&")
+    response_hash = {}
+    pairs.each do |pair|
+      key, value = pair.split("=")
+      response_hash[key] = value
+    end
+    token = response_hash["access_token"]
+    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+    auth = JSON.parse(oauth_response.body)
+    github_token  = GithubToken.find_or_create_by(user_id: current_user.id)
+    github_token.user_id = current_user.id
+    github_token.token = token
+    github_token.save!
+  
+    redirect_to '/dashboard'
   end
+end
--- app/models/github_token.rb
+++ app/models/github_token.rb
@@ -1,3 +1,12 @@
 class GithubToken < ApplicationRecord
   belongs_to :user
+  
+  # def self.find_or_create_from_auth_hash(auth)
+	# 	where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |github_token|
+  #     github_token.user_id  = current_user.id
+  #     github_token.uid      = auth["id"]
+  #     github_token.token    = token
+	# 		github_token.save!
+	# 	end
+	# end
 end
