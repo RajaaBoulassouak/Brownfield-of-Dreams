4d00604ec2d139d63052e3704242a684ff533330
Harper Bellows <harper.bellows@gmail.com>
2018-12-07 08:10:31 -0700

    move the method to save github credential info to the github token model out of user

--- app/models/github_token.rb
+++ app/models/github_token.rb
@@ -4,4 +4,8 @@ class GithubToken < ApplicationRecord
   def self.find_with_omniauth(auth)
     find_by(uid: auth['uid'], provider: auth['provider'])
   end
+
+  def self.create_with_omniauth(auth)
+    create(uid: auth['uid'], provider: auth['provider'])
+  end
 end
