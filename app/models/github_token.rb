0f47b7740cfc038db316cae25b7e138245f7a3eb
Harper Bellows <harper.bellows@gmail.com>
2018-12-07 07:59:00 -0700

    add a method where the sessions#create can find a user by uid and provider

--- app/models/github_token.rb
+++ app/models/github_token.rb
@@ -4,8 +4,4 @@ class GithubToken < ApplicationRecord
   def self.find_with_omniauth(auth)
     find_by(uid: auth['uid'], provider: auth['provider'])
   end
-
-  def self.create_with_omniauth(auth)
-    create(uid: auth['uid'], provider: auth['provider'])
-  end
 end
