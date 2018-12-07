acab6c87daab0a14bd693ad3b641dc8850f07f9c
Harper Bellows <harper.bellows@gmail.com>
2018-12-07 07:57:45 -0700

    add additional logic to handle user sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,20 +5,32 @@ class ApplicationController < ActionController::Base
   helper_method :tutorial_name
 
   add_flash_types :success
-
+  
+  def find_bookmark(id)
+    current_user.user_videos.find_by(video_id: id)
+  end
+  
+  def tutorial_name(id)
+    Tutorial.find(id).title
+  end
+  
+  def four_oh_four
+    raise ActionController::RoutingError.new('Not Found')
+  end
+  
+  protected
+  
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def find_bookmark(id)
-    current_user.user_videos.find_by(video_id: id)
+  def signed_in?
+    !!current_user
   end
+  helper_method :current_user, :signed_in?
 
-  def tutorial_name(id)
-    Tutorial.find(id).title
-  end
-
-  def four_oh_four
-    raise ActionController::RoutingError.new('Not Found')
+  def current_user=(user)
+    session[:user_id] = user&.id
+    @current_user = user
   end
 end
