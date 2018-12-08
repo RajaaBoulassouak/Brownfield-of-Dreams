bce8cb82945d84cece1d260e3f9f2f53926386f4
Harper Bellows <harper.bellows@gmail.com>
2018-12-08 10:30:23 -0700

    add feature test a user can connect this account to their github account

---
+++ spec/features/user/user_can_connect_to_github_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+OmniAuth.config.test_mode = true
+OmniAuth.config.mock_auth[:github] = {
+  "provider" => "GitHub_Mock",
+  "uid" => "12345678910",
+  "credentials" => {
+    "token" => "abcdefg12345"
+  }
+}
+
+feature 'GitHub OmniAuth' do
+  scenario 'user can connect to Github' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/dashboard'
+ 
+    click_link 'Connect to Github'
+
+    # expect(page).to_not have_button('Connect to Github')
+    
+    expect(page).to have_content('Repositories')
+    expect(page).to have_content('Followers')
+  end
+end
