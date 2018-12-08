8cff6b7e643276f6aa25c0d15560e749a76a501a
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-07 17:42:23 -0700

    adds create method to github/sessions_controller github_token model method to find existing or create new github_token instance from github auth data

--- app/models/github_token.rb
+++ app/models/github_token.rb
@@ -1,12 +1,12 @@
 class GithubToken < ApplicationRecord
   belongs_to :user
   
-  # def self.find_or_create_from_auth_hash(auth)
-	# 	where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |github_token|
-  #     github_token.user_id  = current_user.id
-  #     github_token.uid      = auth["id"]
-  #     github_token.token    = token
-	# 		github_token.save!
-	# 	end
-	# end
+  def self.find_or_create_from_auth(auth)
+		where(gh_user_id: auth['uid']).first_or_initialize.tap do |github_token|
+      github_token.gh_user_id = auth['uid']
+      github_token.user_id = User.current.id
+      github_token.token = auth["credentials"]["token"]
+      github_token.save!
+		end
+	end
 end
