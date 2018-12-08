8df1aea33092f4ebedd549f18f8a50dd021a36a6
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-07 17:37:48 -0700

    app/controllers/github/sessions_controller.rb

--- app/controllers/github/sessions_controller.rb
+++ app/controllers/github/sessions_controller.rb
@@ -1,21 +1,9 @@
 class Github::SessionsController < ApplicationController
   
   def create
-    response = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{ENV['GITHUB_CLIENT_ID']}&client_secret=#{ENV['GITHUB_CLIENT_SECRET']}&code=#{params[:code]}")
-    pairs = response.body.split("&")
-    response_hash = {}
-    pairs.each do |pair|
-      key, value = pair.split("=")
-      response_hash[key] = value
-    end
-    token = response_hash["access_token"]
-    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
-    auth = JSON.parse(oauth_response.body)
-    github_token  = GithubToken.find_or_create_by(user_id: current_user.id)
-    github_token.user_id = current_user.id
-    github_token.token = token
-    github_token.save!
-  
+    auth = request.env["omniauth.auth"]
+    GithubToken.find_or_create_from_auth(auth)    
     redirect_to '/dashboard'
   end
 end
+
