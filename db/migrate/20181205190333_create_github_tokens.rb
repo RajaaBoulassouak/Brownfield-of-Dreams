8862fbd1181156109140c43ecb4300f288dc5326
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-07 17:27:49 -0700

    adds gh_user_id column to github_token table and updates schema

--- db/migrate/20181205190333_create_github_tokens.rb
+++ db/migrate/20181205190333_create_github_tokens.rb
@@ -1,6 +1,7 @@
 class CreateGithubTokens < ActiveRecord::Migration[5.2]
   def change
     create_table :github_tokens do |t|
+      t.integer :gh_user_id
       t.string :token
       t.references :user, foreign_key: true
 
