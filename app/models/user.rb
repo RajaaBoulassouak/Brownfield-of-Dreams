277afc5c9ddcd7ad4a65eb43c577cf4adae45c0a
Harper Bellows <harper.bellows@gmail.com>
2018-12-08 16:29:17 -0700

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,8 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
   has_one :github_token
 
-  has_many :followings
-  has_many :followed_users, through: :followings
-
-  has_many :followers, foreign_key: :followed_user_id, class_name: 'Following'
-  has_many :follower_users, through: :followers, source: :user
+  has_many :friends, 
+  has_many :friended_users, through: :friends
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password
