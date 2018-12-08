d28431ae1d1ab42a7590f3fad4d4fd6236bd5328
Harper Bellows <harper.bellows@gmail.com>
2018-12-08 10:28:02 -0700

    add skeleton of a model test for generating a github token from the auth hash

--- spec/models/github_token_spec.rb
+++ spec/models/github_token_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe GithubToken, type: :model do
   describe 'Relationships' do 
     it {should belong_to :user}
   end 
+
+  xit "creates or updates itself from an oauth hash" do    
+    auth = {
+      "provider" => "GitHub_Mock",
+      "uid" => "12345678910",
+      "credentials" => {
+        "token" => "abcdefg12345"
+      }
+    }
+    
+    token = GithubToken.find_or_create_from_auth(auth)
+    user = User.create(first_name: 'Grace', last_name: 'Hopper', email: 'email@email.com', password: 'wonderwoman')
+
+    expect(new_token.gh_user_id).to eq("12345678910")
+    expect(new_token.user_id).to eq(user.id)
+    expect(new_token.token).to eq("abcdefg12345")
+    expect(new_token.provider).to eq("GitHub_Mock")
+  end
 end
