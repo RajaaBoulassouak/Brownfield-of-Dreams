b2d9d02f5f42fbd376d3fc6f84058bd613b8d48a
Harper Bellows <harper.bellows@gmail.com>
2018-12-08 10:31:01 -0700

    adds no new code - removing pry and testing config

--- app/controllers/github/sessions_controller.rb
+++ app/controllers/github/sessions_controller.rb
@@ -2,7 +2,7 @@ class Github::SessionsController < ApplicationController
   
   def create
     auth = request.env["omniauth.auth"]
-    GithubToken.find_or_create_from_auth(auth)    
+    GithubToken.find_or_create_from_auth(auth)
     redirect_to '/dashboard'
   end
 end
