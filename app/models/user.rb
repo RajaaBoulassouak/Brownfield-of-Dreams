30d4893de416cd284d752c13a334abe563bad96b
Harper Bellows <harper.bellows@gmail.com>
2018-12-08 15:52:40 -0700

    add self-referential relationship for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
   has_one :github_token
 
+  has_many :followings
+  has_many :followed_users, through: :followings
+
+  has_many :followers, foreign_key: :followed_user_id, class_name: 'Following'
+  has_many :follower_users, through: :followers, source: :user
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password
   validates_presence_of :first_name
