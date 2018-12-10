f8e1c41372017c9dcb2f612540269c329d9d336b
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-09 17:13:25 -0700

    adds content for creating extra gh_user columns, adds relationships to friendship table and adds self-referantial many relationship for inverse friendships

---
+++ app/models/gh_user.rb
@@ -0,0 +1,18 @@
+class GhUser < ApplicationRecord
+  belongs_to :user
+  has_many :friendships
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverse_friends, through: :inverse_friendships, source: :gh_user
+  
+  def self.find_or_create_from_auth(auth)
+		where(gh_id: auth['uid']).first_or_initialize.tap do |gh_user|
+      gh_user.gh_id   = auth['uid']
+      gh_user.name    = auth["info"]["nickname"]
+      gh_user.link    = auth["extra"]["raw_info"]["html_url"]
+      gh_user.user_id = User.current.id
+      gh_user.token   = auth["credentials"]["token"]
+      gh_user.save!
+		end
+	end
+end
