b401c810f7bd769eee023d4e5ef459c69b1200a8
Harper Bellows <harper.bellows@gmail.com>
2018-12-11 12:15:51 -0700

    wip

--- spec/features/user/user_can_authorize_through_github_spec.rb
+++ spec/features/user/user_can_authorize_through_github_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 feature 'GitHub OmniAuth' do
   it 'user can authorize through Github' do
-    stub_omniauth
-    user = create(:user, gh_id: 37811063) 
-
+    user = create(:user) 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     
     visit '/dashboard'
     
     click_on('Connect to Github')
-
+    
+    stub_omniauth
+    
     expect(current_path).to eq(dashboard_path)
     expect(page).to_not have_button('Connect to Github')
     expect(page).to have_content('Repositories')
