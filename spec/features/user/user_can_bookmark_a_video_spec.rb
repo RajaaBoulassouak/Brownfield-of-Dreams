8f933bb129c7e33b07b8c48e7be697e8531d6df1
Harper Bellows <harper.bellows@gmail.com>
2018-12-11 19:02:29 -0700

    add passing feature test for displaying current user bookmarked segements

--- spec/features/user/user_can_bookmark_a_video_spec.rb
+++ spec/features/user/user_can_bookmark_a_video_spec.rb
@@ -31,4 +31,41 @@ describe 'A registered user' do
     click_on 'Bookmark'
     expect(page).to have_content("Already in your bookmarks")
   end
+
+  it "display user\'s bookmarks grouped by tutorial description and ordered by video position" do
+    tutorial_1= create(:tutorial)
+    tutorial_2= create(:tutorial)
+
+    video_1 = create(:video, tutorial_id: tutorial_1.id)
+    video_2 = create(:video, tutorial_id: tutorial_2.id)
+
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit tutorial_path(tutorial_1)
+
+    click_on 'Bookmark'
+    
+    visit tutorial_path(tutorial_2)
+
+    click_on 'Bookmark'
+
+     visit '/dashboard'
+
+     expect(page).to have_content('Bookmarked Segments')
+
+     within(".tutorial-#{tutorial_1.id}") do
+      expect(page).to have_content(tutorial_1.description)
+    end
+     within(".video-#{video_1.id}") do
+      expect(page).to have_content(video_1.title)
+    end
+     within(".tutorial-#{tutorial_2.id}") do
+      expect(page).to have_content(tutorial_2.description)
+    end
+     within(".video-#{video_2.id}") do
+      expect(page).to have_content(video_2.title)
+    end
+  end
 end
