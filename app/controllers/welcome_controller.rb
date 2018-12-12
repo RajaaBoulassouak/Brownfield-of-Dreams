dd4b081fbc753c1d8999930121483442836581b5
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-11 18:04:04 -0700

    adds controller logic to hide classroom content tutorials from not logged in users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
+    if params[:tag] && current_user
       @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-    else
+    elsif current_user
       @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+    elsif params[:tag]
+      @tutorials = Tutorial.not_classroom_content.agged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+    else
+      @tutorials = Tutorial.not_classroom_content.paginate(:page => params[:page], :per_page => 5)
     end
   end
 end
