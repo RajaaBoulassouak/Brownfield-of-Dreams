b0ffbbe36ae7527a3ffcf9528490bf4a3d62d6e8
Harper Bellows <harper.bellows@gmail.com>
2018-12-12 18:07:16 -0700

    add VCR to mock feature tests that hit the GitHub API

--- spec/features/user/user_can_see_github_repos_spec.rb
+++ spec/features/user/user_can_see_github_repos_spec.rb
@@ -2,38 +2,42 @@ require 'rails_helper'
 
 feature 'User can see a list of 5 of their Github repositories' do
   scenario 'with the name of each Repo linking to the repo on Github' do
-    user = create(:user)
-    GhUser.create(token: ENV['USER_GITHUB_TOKEN_1'], user_id: user.id)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    VCR.use_cassette('GitHub Feature User Sees Their Repos') do
+      user = create(:user)
+      GhUser.create(token: ENV['USER_GITHUB_TOKEN_1'], user_id: user.id)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit '/dashboard'
+      visit '/dashboard'
 
-    expect(current_path).to eq('/dashboard')
-    expect(page).to have_content('Github')
-    expect(page).to have_content('Repositories')
-    expect(page).to have_css('.repo', count: 5)
-    within(first('.repo')) do 
-      expect(page).to have_css('.name')
-      expect(page).to have_link('trelora_consult_app')
+      expect(current_path).to eq('/dashboard')
+      expect(page).to have_content('Github')
+      expect(page).to have_content('Repositories')
+      expect(page).to have_css('.repo', count: 5)
+      within(first('.repo')) do 
+        expect(page).to have_css('.name')
+        expect(page).to have_link('trelora_consult_app')
+      end
     end
   end
 end
 
 feature 'A different user can see a list of 5 of their Github repositories' do
   scenario 'with the name of each Repo linking to the repo on Github' do
-    user = create(:user)
-    GhUser.create(token: ENV['USER_GITHUB_TOKEN_2'], user_id: user.id)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    VCR.use_cassette('GitHub Feature A Different User Sees Their Repos') do
+      user = create(:user)
+      GhUser.create(token: ENV['USER_GITHUB_TOKEN_2'], user_id: user.id)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit '/dashboard'
+      visit '/dashboard'
 
-    expect(current_path).to eq('/dashboard')
-    expect(page).to have_content('Github')
-    expect(page).to have_content('Repositories')
-    expect(page).to have_css('.repo', count: 5)
-    within(first('.repo')) do 
-      expect(page).to have_css('.name')
-      expect(page).to have_link('night_writer')
+      expect(current_path).to eq('/dashboard')
+      expect(page).to have_content('Github')
+      expect(page).to have_content('Repositories')
+      expect(page).to have_css('.repo', count: 5)
+      within(first('.repo')) do 
+        expect(page).to have_css('.name')
+        expect(page).to have_link('night_writer')
+      end
     end
   end
 end
