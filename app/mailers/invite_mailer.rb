4f8958c938c1e571e6d9b7f47fdf536c3a5048ab
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-13 19:11:45 -0700

    adds tests for user mailer

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -3,7 +3,6 @@ class InviteMailer < ApplicationMailer
   def invitation(github_member, current_user)
     @user = current_user
     @github_member = github_member
-
     mail(to: github_member.email, subject: 'Turing Tutorial Invitation')
   end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,21 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-  
+  describe 'account_activation' do
+    before(:each) do
+      @user = create(:user)
+    end
+    let(:email) { UserMailer.account_activation(@user) }
+    
+    it 'renders headers' do
+      expect(email.subject).to eq('Account Activation')
+      expect(email.to).to eq([@user.email])
+      expect(email.from).to eq(['no-reply@activate.io'])
+    end
+
+    it 'renders body' do
+      expect(email.body.encoded).to match('Hello')
+      expect(email.body.encoded).to include(@user.activation_token)
+    end
+  end
 end
